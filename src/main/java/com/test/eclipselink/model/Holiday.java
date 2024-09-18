663a9f435f63e616a1c074a607fd081f98fb388a
Antonio Damato <adamato@Antonios-MacBook-Pro.local>
2024-09-18 15:32:07 +0100

    named query tests

--- src/main/java/com/test/eclipselink/model/Holiday.java
+++ src/main/java/com/test/eclipselink/model/Holiday.java
@@ -2,58 +2,57 @@ package com.test.eclipselink.model;
 
 import java.time.LocalDate;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "checkInPeriod", query = "select o from Holiday o where o.checkIn between :dateStart and :dateEnd")
+})
 public class Holiday {
-	@SequenceGenerator(name = "SEQ_HOLIDAY", sequenceName = "SEQ_GEN_SEQUENCE", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_HOLIDAY")
-	@Id
-	private Long id;
+    @SequenceGenerator(name = "SEQ_HOLIDAY", sequenceName = "SEQ_GEN_SEQUENCE", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_HOLIDAY")
+    @Id
+    private Long id;
 
-	private Integer travellers;
-	private LocalDate checkIn;
-	private Integer nights;
-	private String referenceName;
+    private Integer travellers;
+    private LocalDate checkIn;
+    private Integer nights;
+    private String referenceName;
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public Integer getTravellers() {
-		return travellers;
-	}
+    public Integer getTravellers() {
+        return travellers;
+    }
 
-	public void setTravellers(Integer travellers) {
-		this.travellers = travellers;
-	}
+    public void setTravellers(Integer travellers) {
+        this.travellers = travellers;
+    }
 
-	public LocalDate getCheckIn() {
-		return checkIn;
-	}
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
 
-	public void setCheckIn(LocalDate checkIn) {
-		this.checkIn = checkIn;
-	}
+    public void setCheckIn(LocalDate checkIn) {
+        this.checkIn = checkIn;
+    }
 
-	public Integer getNights() {
-		return nights;
-	}
+    public Integer getNights() {
+        return nights;
+    }
 
-	public void setNights(Integer nights) {
-		this.nights = nights;
-	}
+    public void setNights(Integer nights) {
+        this.nights = nights;
+    }
 
-	public String getReferenceName() {
-		return referenceName;
-	}
+    public String getReferenceName() {
+        return referenceName;
+    }
 
-	public void setReferenceName(String referenceName) {
-		this.referenceName = referenceName;
-	}
+    public void setReferenceName(String referenceName) {
+        this.referenceName = referenceName;
+    }
 
 }
